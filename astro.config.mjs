26eca8efdfb8ed128997afa115a7919173aed166
sheceido <leonpoon@hotmail.ca>
2023-05-16 22:32:08 -0400

    omit vercel SSR option, change back to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+// import vercel from '@astrojs/vercel/serverless';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercel(),
+	// output: 'server',
+	// adapter: vercel(),
 	integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
